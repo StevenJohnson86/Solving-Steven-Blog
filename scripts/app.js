af390453669a42d44c05a81c5bae409e51ee6c90
Steven Johnson <stevenjohnson86@gmail.com>
2017-02-08 10:10:00 -0800

    added basic files and structure for repo

---
+++ scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function Project (title, descript, img, path) {
+  this.title = title;
+  this.descript = descript;
+  this.img = img;
+  this.path = path;
+  //add timeDate prototype
+}
+
+function BlogEntry (title, content) {
+  this.title = title;
+  this.content = content;
+  //add timeDate prototype
+}
