426363de35271736238072549473afa177b21d5d
Steven Johnson <stevenjohnson86@gmail.com>
2017-02-21 09:37:45 -0800

    created a closure for projectView.js

--- public/scripts/projectView.js
+++ public/scripts/projectView.js
@@ -1,43 +1,45 @@
 'use strict';
+(function(module){
+  let projectView = {};
 
-let projectView = {};
+  projectView.populateFilter = function () {
+    $('.public-project').each(function(){
+      let title, optionTag;
+      title = $(this).find('h2').text();
+      optionTag = `<option value="${title}">${title}</option>`;
 
-projectView.populateFilter = function () {
-  $('.public-project').each(function(){
-    let title, optionTag;
-    title = $(this).find('h2').text();
-    optionTag = `<option value="${title}">${title}</option>`;
+      if ($(`#title-filter option[value="${title}"]`).length === 0) {
+        $('#title-filter').append(optionTag);
+      }
+    });
+  }
 
-    if ($(`#title-filter option[value="${title}"]`).length === 0) {
-      $('#title-filter').append(optionTag);
-    }
-  });
-}
+  projectView.handleTitleFilter = function() {
+    $('#title-filter').on('change', function() {
+      if ($(this).val()) {
+        $('.public-project').hide();
+        $('div[data-title = "' + $(this).val() + '"]').fadeIn();
+      } else {
+        $('.public-project').fadeIn();
+      }
+    });
+  }
 
-projectView.handleTitleFilter = function() {
-  $('#title-filter').on('change', function() {
-    if ($(this).val()) {
-      $('.public-project').hide();
-      $('div[data-title = "' + $(this).val() + '"]').fadeIn();
-    } else {
-      $('.public-project').fadeIn();
-    }
-  });
-}
+  projectView.handleNav = function () {
+    $('.main-nav').on('click', '.tab', function() {
+      $('section').hide();
+      $('#current-loc').text($(this).text());
+      $('#' + $(this).data('content') + '-content').fadeIn();
+    });
+  };
 
-projectView.handleNav = function () {
-  $('.main-nav').on('click', '.tab', function() {
-    $('section').hide();
-    $('#current-loc').text($(this).text());
-    $('#' + $(this).data('content') + '-content').fadeIn();
-  });
-};
-
-projectView.initIndex = function () {
-  Project.all.forEach(function (p) {
-    $('#projects-content').append(p.toHtml());
-  });
-  projectView.populateFilter();
-  projectView.handleTitleFilter();
-  projectView.handleNav();
-}
+  projectView.initIndex = function () {
+    Project.all.forEach(function (p) {
+      $('#projects-content').append(p.toHtml());
+    });
+    projectView.populateFilter();
+    projectView.handleTitleFilter();
+    projectView.handleNav();
+  }
+  module.projectView = projectView;
+})(window);
